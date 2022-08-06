efc46332d8f5bc28bfecfbcc76421eb3c6617d5c
Antonie433 <88628184+Antonie433@users.noreply.github.com>
2022-08-06 21:07:45 +0100

    Update settings.py

--- technocolabProject/settings.py
+++ technocolabProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
-
+import os
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
