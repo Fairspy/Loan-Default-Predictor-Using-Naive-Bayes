77434e1606bca3b32dd1fcaa552e9c05a4496ca6
fairspy <honeymishram@gmail.com>
2022-08-07 01:26:16 +0530

    update

--- LoanDefaultPredictorNB/predictions.py
+++ LoanDefaultPredictorNB/predictions.py
@@ -7,8 +7,8 @@ def make_predictions(LoanDayDel,CustomerPrincipalPayments,GrossPrincipalLoss,Cus
     print(BASE)
     x = [LoanDayDel,CustomerPrincipalPayments,GrossPrincipalLoss,CustomerPay,InterestFees,ServiceFee,MLoanPay,AvailBCredit,RevCreditBal]
     x_shaped = np.array(x).reshape(1,-1)
-    tf = pickle.load(open(os.path.join(BASE,'../static/PowerTransform.pkl'),'rb'))
+    tf = pickle.load(open(os.path.join(BASE,'static/PowerTransform.pkl'),'rb'))
     x_final = tf.transform(x_shaped)
-    model = pickle.load(open(os.path.join(BASE,'../static/model_final_NB.pkl'),'rb'))
+    model = pickle.load(open(os.path.join(BASE,'static/model_final_NB.pkl'),'rb'))
     prediction = model.predict(x_final)
     return prediction
